c5640901bcaa6815293f6449104b6c07574e7dfc
Adebayo Adeniyan <byoextreme@hotmail.com>
2025-12-31 16:08:22 +0100

    tsk(git): init project

---
+++ src/main/java/com/ad3bayo/kafkabasic/KafkabasicApplication.java
@@ -0,0 +1,13 @@
+package com.ad3bayo.kafkabasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkabasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkabasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ad3bayo/kafkabasic/KafkabasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad3bayo.kafkabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkabasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
